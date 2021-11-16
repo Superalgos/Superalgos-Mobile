f736c6c1f401a0e70def01b5e0432a027d1b658d
Alexandru Stefan <stefan.i.alexandru@gmail.com>
2021-11-17 01:14:03 +0200

    first commit

---
+++ android/app/src/main/kotlin/org/superalgos/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.superalgos.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/secure_storage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  final storage = const FlutterSecureStorage();
+
+
+
+}
